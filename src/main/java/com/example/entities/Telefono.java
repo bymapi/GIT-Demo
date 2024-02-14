2bf0c236aca2f9bde800ffd86ca3e88b54311e47
oumaymaBombarek <total.oumayma82@gmail.com>
2024-02-14 13:42:15 +0100

    TelefonoHecho

---
+++ src/main/java/com/example/entities/Telefono.java
@@ -0,0 +1,21 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Telefono implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private String numero;
+
+}
